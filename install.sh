0b4f24147f378269cf8f0389ecd490a3bed59554
jagernau <stepanenkov25@gmail.com>
2024-03-17 21:01:45 +0200

    наведение порядков в env, тест автоустановки, вывод версии докера

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #git clone https://github.com/Jagernau/monitoring_csv -b auto_create monitoring_db_autosave
 
-cd monitoring_db_autosave
+#cd monitoring_db_autosave
 
 #проверить установлен ли Docker
 if ! [ -x "$(command -v docker)" ]; then
