e7c342b898f18218ce6d2585d515d991e26d7640
jagernau <stepanenkov25@gmail.com>
2024-04-18 23:02:47 +0200

    админка настройка nginx

--- data_base_postgres_install.sh
+++ data_base_postgres_install.sh
@@ -149,10 +149,10 @@ events { worker_connections 1024; }
 http {
     server {
         listen 80;
-        server_name localhost;
+        server_name 0.0.0.0;
 
         location / {
-            proxy_pass http://localhost:8000;
+            proxy_pass http://0.0.0.0:8000;
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
